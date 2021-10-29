3b39e037518f649f48f3703e639d358a431b02fb
Jaipal Jadeja <jadejajaipal5@gmail.com>
2021-10-29 13:25:19 +0530

    little cleanup

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
-const notes = require("./data/data");
 const connectDB = require("./config/db");
 
 const userRoutes = require("./routes/userRoutes");
@@ -17,10 +16,6 @@ connectDB();
 
 app.get("/", (req, res) => {
   res.send("Server is running");
-});
-
-app.get("/api/notes", (req, res) => {
-  res.json(notes);
 });
 
 app.use("/api/users", userRoutes);
