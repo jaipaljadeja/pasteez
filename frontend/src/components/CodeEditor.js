0c26782208c78701d20f90c5038d33984d6f1cc2
bBSempai <bbwhacker0805@gmail.com>
2021-11-09 20:00:12 +0530

    button-fix

--- frontend/src/components/CodeEditor.js
+++ frontend/src/components/CodeEditor.js
@@ -78,98 +78,130 @@ export default function CodeEditor({ languages, data, setData, syntaxStyles }) {
     }
   };
 
+  const codeCopyHandler = (code) => {
+    navigator.clipboard.writeText(code);
+    toast.success("Code Copied", {
+      style: {
+        fontFamily: "Monospace",
+        marginTop: "15px",
+      },
+      iconTheme: {
+        primary: "#33b4ff",
+        secondary: "#FFFFFF",
+      },
+    });
+  };
+
   return (
-    <div className="pasteez-editor">
-      <div
-        className="exportableFrame"
-        style={{
-          background: data.framebg,
-        }}
-      >
-        <div className="editor-frame">
-          <div className="frame-header">
-            <div className="frame-header-buttons">
-              <div className="frame-header-circle red-circle"></div>
-              <div className="frame-header-circle yellow-circle"></div>
-              <div className="frame-header-circle green-circle"></div>
+    <div style={{ display: "flex" }}>
+      <div className="pasteez-editor">
+        <div
+          className="exportableFrame"
+          style={{
+            background: data.framebg,
+          }}
+        >
+          <div className="editor-frame">
+            <div className="frame-header">
+              <div className="frame-header-buttons">
+                <div className="frame-header-circle red-circle"></div>
+                <div className="frame-header-circle yellow-circle"></div>
+                <div className="frame-header-circle green-circle"></div>
+              </div>
+              <div className="frame-header-title">
+                <input
+                  type="text"
+                  spellCheck="false"
+                  defaultValue={data.title}
+                  onChange={(e) => {
+                    setData({ ...data, title: e.target.value });
+                  }}
+                />
+              </div>
+              <div className="frame-header-buttons" style={{ opacity: 0 }}>
+                <div className="frame-header-circle"></div>
+                <div className="frame-header-circle"></div>
+                <div className="frame-header-circle"></div>
+              </div>
             </div>
-            <div className="frame-header-title">
-              <input
-                type="text"
-                spellCheck="false"
-                defaultValue={data.title}
-                onChange={(e) => {
-                  setData({ ...data, title: e.target.value });
-                }}
+            <div className="frame-body">
+              <Editor
+                value={data.code}
+                onValueChange={(code) => setData({ ...data, code: code })}
+                highlight={(code) =>
+                  hljs.highlight(code, {
+                    language: data.lang,
+                  }).value
+                }
+                autoFocus={false}
+                padding={20}
+                textareaId="code-editor"
+                className="code-editor-wrapper"
+                style={{ lineHeight: 1.5 }}
               />
             </div>
-            <div className="frame-header-buttons" style={{ opacity: 0 }}>
-              <div className="frame-header-circle"></div>
-              <div className="frame-header-circle"></div>
-              <div className="frame-header-circle"></div>
-            </div>
           </div>
-          <div className="frame-body">
-            <Editor
-              value={data.code}
-              onValueChange={(code) => setData({ ...data, code: code })}
-              highlight={(code) =>
-                hljs.highlight(code, {
-                  language: data.lang,
-                }).value
-              }
-              autoFocus={false}
-              padding={20}
-              textareaId="code-editor"
-              className="code-editor-wrapper"
-              style={{ lineHeight: 1.5 }}
-            />
+        </div>
+        <div className="frame-footer">
+          <div className="frame-footer-buttons">
+            <div className="settings-btn">
+              <Dropdown
+                controlClassName="btn"
+                options={languages}
+                onChange={(e) => {
+                  const lang = e.value;
+                  handleLangChange(lang);
+                }}
+                value={data.lang}
+                placeholder="Select a language"
+              />
+              <Dropdown
+                controlClassName="btn"
+                options={Object.getOwnPropertyNames(syntaxStyles)}
+                onChange={(e) => {
+                  const syntaxTheme = e.value.toLowerCase();
+                  handleSyntaxThemeChange(syntaxTheme);
+                }}
+                value={data.syntaxStyle}
+                placeholder="Select a style"
+              />
+              <button className="btn share-btn" onClick={handleCopyUrl}>
+                Copy Url
+                <i
+                  style={{ marginLeft: "10px", fontSize: "10px" }}
+                  className="far fa-link"
+                ></i>
+              </button>
+              <ColorPicker
+                onChange={(color) => setData({ ...data, framebg: color })}
+              />
+            </div>
+            <div className="features-btn">
+              <button className="btn export-btn" onClick={handleImageExport}>
+                Export
+                <i
+                  style={{ marginLeft: "10px", fontSize: "10px" }}
+                  className="fal fa-file-export"
+                ></i>
+              </button>
+            </div>
           </div>
         </div>
       </div>
-      <div className="frame-footer">
-        <div className="frame-footer-buttons">
-          <div className="settings-btn">
-            <Dropdown
-              controlClassName="btn"
-              options={languages}
-              onChange={(e) => {
-                const lang = e.value;
-                handleLangChange(lang);
-              }}
-              value={data.lang}
-              placeholder="Select a language"
-            />
-            <Dropdown
-              controlClassName="btn"
-              options={Object.getOwnPropertyNames(syntaxStyles)}
-              onChange={(e) => {
-                const syntaxTheme = e.value.toLowerCase();
-                handleSyntaxThemeChange(syntaxTheme);
-              }}
-              value={data.syntaxStyle}
-              placeholder="Select a style"
-            />
-            <button className="btn share-btn" onClick={handleCopyUrl}>
-              Copy Url
-              <i
-                style={{ marginLeft: "10px", fontSize: "10px" }}
-                className="far fa-link"
-              ></i>
-            </button>
-            <ColorPicker
-              onChange={(color) => setData({ ...data, framebg: color })}
-            />
-          </div>
-          <div className="features-btn">
-            <button className="btn export-btn" onClick={handleImageExport}>
-              Export
-              <i
-                style={{ marginLeft: "10px", fontSize: "10px" }}
-                className="fal fa-file-export"
-              ></i>
-            </button>
-          </div>
+      <div className="button-container editor">
+        <div
+          className="btn post-edit-btn clipboard"
+          title={"copy code"}
+          onClick={() => codeCopyHandler(data.code)}
+        >
+          <i className="fas fa-clipboard" />
+        </div>
+        <div
+          className="btn post-edit-btn save"
+          title={"save paste"}
+          // onClick={() => codeCopyHandler(data.code)}
+        >
+          <i className="fas fa-save" />
         </div>
       </div>
     </div>
